b00b798af7fe88346638fdc819754ca8edc8c9c4
Jackietruong2805 <truonghao2805@gmail.com>
2023-03-21 16:49:28 +0700

    Soft Delete and Data Restore

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    public function all(){
+        $students = Student::get();
+        dd($students);
+    }
+    public function delete($id){
+        Student::where('id', $id)->delete();
+    }
+    public function trashed(){
+        return dd(Student::onlyTrashed()->latest()->get());
+    }
+    public function restore($id){
+       Student::where('id', $id)->restore();
+    }
+}
